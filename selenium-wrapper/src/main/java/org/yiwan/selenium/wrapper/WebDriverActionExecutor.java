6d4f7480b288a5682429e574f0c981dac3a83962
Kenny Wang <kenny.wang@lombardrisk.com>
2017-02-08 17:56:35 +0800

    add new property action.max.retry.times=2

--- selenium-wrapper/src/main/java/org/yiwan/selenium/wrapper/WebDriverActionExecutor.java
+++ selenium-wrapper/src/main/java/org/yiwan/selenium/wrapper/WebDriverActionExecutor.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yiwan.easy.util.ActionExecutor;
 import org.yiwan.easy.util.IAction;
+import org.yiwan.easy.util.PropertiesHelper;
 
 /**
  * Created by Kenny Wang on 8/22/2016.
@@ -14,7 +15,7 @@ public class WebDriverActionExecutor extends ActionExecutor {
     private final Logger logger = LoggerFactory.getLogger(WebDriverActionExecutor.class);
 
     public WebDriverActionExecutor() {
-        super(2);
+        super(Integer.parseInt(PropertiesHelper.getProperty("action.max.retry.times")));
     }
 
     public WebDriverActionExecutor(int max_retry_times) {
